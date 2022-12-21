8d90f10780f8d27cf0905aa833cb99b7da5a25cd
Michael <michaelshomefolder@Michaels-Laptop.local>
2022-12-21 22:14:49 +0800

    feat: major update of application

---
+++ database/models.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// issue schema
+const IssueSchema = new Schema({
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_on: Date,
+    updated_on: Date,
+    created_by: { type: String, required: true },
+    assigned_to: String,
+    open: Boolean,
+    status_text: String
+});
+// issue model
+const Issue = mongoose.model("Issue", IssueSchema);
+
+// project schema
+const ProjectSchema = new Schema({
+    name: { type: String, required: true },
+    issues: [IssueSchema]
+});
+// project model
+const Project = mongoose.model("Project", ProjectSchema);
+
+
+// export models
+exports.Issue = Issue;
+exports.Project = Project;
